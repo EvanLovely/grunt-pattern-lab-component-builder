be05c257efb9bb5379d12da5aa6137ee8eb0056e
Evan Lovely <evan@evanlovely.com>
2014-08-06 14:33:12 -0700

    fixed bump config

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,22 @@ module.exports = function (grunt) {
     // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js']
+    },
+
+    bump: {// https://github.com/vojtajina/grunt-bump
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
     }
 
   });
