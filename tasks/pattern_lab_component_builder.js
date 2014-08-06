520dac42aaf8c34baf3e17d79eeeabc2bb90fdba
Evan Lovely <evan@evanlovely.com>
2014-08-06 16:14:36 -0700

    adding uses of colors

--- tasks/pattern_lab_component_builder.js
+++ tasks/pattern_lab_component_builder.js
@@ -7,9 +7,8 @@
  */
 
 'use strict';
-var fs = require('fs');
-var util = require('util');
 var Mustache = require('Mustache');
+var _ = require('underscore-node');
 module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
@@ -19,7 +18,7 @@ module.exports = function (grunt) {
 
     // Merge task-specific and/or target-specific options with these defaults.
     var $options = this.options({
-      'regex': /^\$color.*/mg,
+      'regex': /^\$color--.*/mg,
       'template': 'templates/colors.mustache'
     });
 
@@ -37,29 +36,42 @@ module.exports = function (grunt) {
           grunt.fail.fatal("No matching items found in `" + $file.src + "` while searching it with the RegEx `" + $options.regex + '`\n' + "Consider running again with --verbose");
         }
 
-        var $colors = [];
+        var $colors = [],
+            $colors_with_var_values = [];
+
         $sass_filtered.forEach(function($color) {
           var $name = $color.split(":")[0];
           var $value = $color.split(":")[1].trim().replace(";", "");
-          $colors.push({"name": $name, "value": $value});
+          if (!$value.match(/\$/)){
+            $colors.push({"name": $name, "value": $value, "uses": []});
+          } else {
+            $colors_with_var_values.push({"name": $name, "value": $value});
+          }
           grunt.log.verbose.writeln("Name : " + $name);
           grunt.log.verbose.writeln("Value: " + $value);
         });
 
-        // @todo The string `"colors"` below needs to be replaced with `$target`, but isn't working
         var $target = grunt.task.current.target;
-        var $plInfo = {
-          "colors": $colors
-        };
+        var $plInfo = {};
+        $plInfo[$target] = $colors;
+
+        $colors_with_var_values.forEach(function($varColor) {
+          $plInfo[$target].forEach(function($color) {
+            if ($color.name == $varColor.value) {
+              $color["uses"].push($varColor.name);
+            }
+          });
+        });
 
         var $template = grunt.file.read($options.template);
         var $plComponent = Mustache.render($template, $plInfo);
         grunt.file.write($file.dest, $plComponent);
         grunt.log.ok("Rendered!");
 
-
-        grunt.log.verbose.subhead("PL Info JSON:");
-        grunt.log.verbose.writeln(JSON.stringify($plInfo, null, '\t'));
+        grunt.log.subhead("PL Info JSON:");
+        grunt.log.writeln(JSON.stringify($plInfo, null, '\t'));
+        grunt.log.subhead("Colors with Var Values:");
+        grunt.log.writeln(JSON.stringify($colors_with_var_values, null, '\t'));
         grunt.log.verbose.subhead("Template Contents:");
         grunt.log.verbose.writeln($template);
         grunt.log.verbose.subhead("View Contents:");
