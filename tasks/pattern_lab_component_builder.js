ba3364499e7e19742b705f71dd57896c21ee7d78
Evan Lovely <evan@evanlovely.com>
2014-08-07 16:48:05 -0700

    dev back to master

--- tasks/pattern_lab_component_builder.js
+++ tasks/pattern_lab_component_builder.js
@@ -17,86 +17,51 @@ module.exports = function (grunt) {
   grunt.registerMultiTask('pattern_lab_component_builder', 'Automatically Create Pattern Lab Components', function () {
 
     // Merge task-specific and/or target-specific options with these defaults.
-    var
-      $options = this.options({
-//        'regex': /^\$color--.*/mg,
-//        'template': 'templates/colors.mustache'
-      }),
-      $target = this.target
-    ;
+    var $options = this.options({
+      'regex': /^\$color--.*/mg,
+      'template': 'templates/colors.mustache'
+    });
 
     // Iterate over all specified file groups.
     this.files.forEach(function ($file) {
+      if ($options.component === "colors") {
         grunt.log.writeln("Source File: " + $file.src);
         grunt.log.writeln("Template File: " + $options.template);
         grunt.log.writeln("Destination File: " + $file.dest);
 
-      var get = function($fileSrc) {
-        var $contents = grunt.file.read($fileSrc);
-//        var $results = $contents.match(/./);
-        var $regex = new RegExp("^\\$" + $options.regex, "mg");
-//        var $regex = new RegExp($options.regex, "mg");
-        var $matched = $contents.match($regex);
-        if ($matched == null) {
-          grunt.fail.fatal("No matching items found in `" + $fileSrc + "` while searching it with the RegEx `" + $options.regex + '`\n' + "Consider running again with --verbose");
+        var $sass = grunt.file.read($file.src);
+        var $regExForSassVars = $options.regex;
+        var $sass_filtered = $sass.match($regExForSassVars);
+        if ($sass_filtered == null) {
+          grunt.fail.fatal("No matching items found in `" + $file.src + "` while searching it with the RegEx `" + $options.regex + '`\n' + "Consider running again with --verbose");
         }
-//        return $results;
-        var
-          $results = [],
-          $results_with_var_values = []
-        ;
-        $matched.forEach(function($item) {
-          var $name = $item.split(":")[0];
-          var $value = $item.split(":")[1].trim().replace(";", "");
+
+        var $colors = [],
+          $colors_with_var_values = [];
+
+        $sass_filtered.forEach(function ($color) {
+          var $name = $color.split(":")[0];
+          var $value = $color.split(":")[1].trim().replace(";", "");
           if (!$value.match(/\$/)) {
-            $results.push({"name": $name, "value": $value, "uses": []});
+            $colors.push({"name": $name, "value": $value, "uses": []});
           } else {
-            $results_with_var_values.push({"name": $name, "value": $value});
+            $colors_with_var_values.push({"name": $name, "value": $value});
           }
           grunt.log.verbose.writeln("Name : " + $name);
           grunt.log.verbose.writeln("Value: " + $value);
         });
-        return $results;
-      };
 
-      if ($options.component === "fonts") {
-
-        var $fonts = [];
-        grunt.log.subhead("Contents of Fonts:");
-//        grunt.log.writeln($contents);
-        get($file.src).forEach(function($font) {
-          grunt.log.writeln("$font: " + $font);
-        });
-      }
-
-      if ($options.component === "colors") {
-
-        var $colors = [],
-            $colors_with_var_values = [];
-        $colors.push(get($file.src));
-//        get($file.src).forEach(function($color) {
-//          var $name = $color.split(":")[0];
-//          var $value = $color.split(":")[1].trim().replace(";", "");
-//          if (!$value.match(/\$/)){
-//            $colors.push({"name": $name, "value": $value, "uses": []});
-//          } else {
-//            $colors_with_var_values.push({"name": $name, "value": $value});
-//          }
-//          grunt.log.verbose.writeln("Name : " + $name);
-//          grunt.log.verbose.writeln("Value: " + $value);
-//        });
-          grunt.log.verbose.writeln("$colors: " + JSON.stringify($colors, null, '\t'));
-
+        var $target = grunt.task.current.target;
         var $plInfo = {};
         $plInfo[$target] = $colors;
 
-//        $colors_with_var_values.forEach(function($varColor) {
-//          $plInfo[$target].forEach(function($color) {
-//            if ($color.name === $varColor.value) {
-//              $color["uses"].push($varColor.name);
-//            }
-//          });
-//        });
+        $colors_with_var_values.forEach(function ($varColor) {
+          $plInfo[$target].forEach(function ($color) {
+            if ($color.name === $varColor.value) {
+              $color["uses"].push($varColor.name);
+            }
+          });
+        });
 
         var $template = grunt.file.read($options.template);
         var $plComponent = mustache.render($template, $plInfo);
